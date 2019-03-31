bde412dd1b0f233bdc457cdf099e66af2c1b0d36
shubham15098 <shubham15098@iiitd.ac.in>
2019-03-31 19:45:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shubzz/recylerviewclickable/MyListData.java
@@ -0,0 +1,34 @@
+package com.example.shubzz.recylerviewclickable;
+
+/**
+ * Created by shubzz on 31/3/19.
+ */
+
+public class MyListData
+{
+    private String description;
+    private int imgId;
+
+    public MyListData(String description, int imgId)
+    {
+        this.description = description;
+        this.imgId = imgId;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    public int getImgId()
+    {
+        return imgId;
+    }
+    public void setImgId(int imgId)
+    {
+        this.imgId = imgId;
+    }
+}
+
